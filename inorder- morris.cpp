c7570d853cd13a6bcf0a136dba10375dcbaa6106
priety33 <52635177+priety33@users.noreply.github.com>
2020-03-30 02:30:36 +0530

    Update inorder- morris.cpp

--- inorder- morris.cpp
+++ inorder- morris.cpp
@@ -19,7 +19,7 @@ public:
             if(!current->left)
             {
                 v.push_back(current->val);
-                current=current->right;
+                current=current->right; //traversing up from the threaded link
             }
             else
             {
@@ -31,13 +31,14 @@ public:
                 
                 if(!pre->right)
                 {
-                    pre->right=current;
+                    pre->right=current; //creating a thread from inorder predecessor to current node => only after this node 
+                                        //is printed (meaning whole left subtree is printed), will current get printed
                     current=current->left;
                 }
                 else
                 {
-                    pre->right=NULL; //removing threaded connections
-                    v.push_back(current->val);
+                    pre->right=NULL; //removing threaded connections => whole left subtree of current is printed
+                    v.push_back(current->val);// now current has to printed, after the left subtree
                     current=current->right;
                 }
             }
