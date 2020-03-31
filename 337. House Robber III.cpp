7d107f3766e9fb66235743b6e92a27886667be18
priety33 <52635177+priety33@users.noreply.github.com>
2020-03-31 12:30:41 +0530

    Update 337. House Robber III.cpp

--- 337. House Robber III.cpp
+++ 337. House Robber III.cpp
@@ -23,3 +23,26 @@ public:
         return max(root->val+ll+lr+rr+rl, lsum+rsum);
     }
 };
+
+
+
+//DP APPROACH
+class Solution {
+public:
+    //lm, rm are immediate children
+    int helper(TreeNode* root,int& lm,int& rm){ //children are passed by reference so that when we call on children, 
+                                                 //we get the value of grandchildren also
+        if(root==NULL) return 0;
+        int ll=0,lr=0,rr=0,rl=0; //these are the grandchilren
+        lm=helper(root->left,ll,lr); //its grandchildren-> children of it's immediate child
+        rm=helper(root->right,rl,rr);
+        cout << root->val << " " << lm <<" " << rm << " "<< ll << " " << lr << " " << rl << " " << rr << endl;
+        return max(root->val+ll+lr+rr+rl,lm+rm);
+    }
+    int rob(TreeNode* root) {
+        if(root==NULL) return 0;
+        int lm=0,rm=0;
+        int res=helper(root,lm,rm);
+        return res;
+    }
+};
