c1df9ce747520f54ae25f9bc4e5de42a01ba5d26
priety33 <52635177+priety33@users.noreply.github.com>
2020-04-01 15:16:43 +0530

    Create 450. Delete Node in a BST.cpp

---
+++ 450. Delete Node in a BST.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root) return NULL;
+        
+        if(key>root->val) root->right=deleteNode(root->right, key);
+        else if(key<root->val) root->left=deleteNode(root->left, key);
+        else
+        {
+            if(!root->left && !root->right) return NULL;
+            else if(!root->left) return root->right;
+            else if(!root->right) return root->left;
+            else
+            {
+                TreeNode* smallest=root->right;  //finding smallest value in right subtree to replace current root
+                while(smallest->left) smallest=smallest->left;
+                
+                //replace
+                root->val=smallest->val;
+                //delete this smallest value in right subtree
+                root->right=deleteNode(root->right, smallest->val);
+            }
+        }
+        return root;   
+    }
+};
