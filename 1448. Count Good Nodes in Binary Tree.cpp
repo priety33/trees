bf4d04cb7e049abc96a0d1caf835ab6d4427781e
priety33 <52635177+priety33@users.noreply.github.com>
2020-05-17 11:24:33 +0530

    Update 1448. Count Good Nodes in Binary Tree.cpp

--- 1448. Count Good Nodes in Binary Tree.cpp
+++ 1448. Count Good Nodes in Binary Tree.cpp
@@ -20,3 +20,13 @@ public:
         return ans;
     }
 };
+
+
+//OR just keep count of maximum val
+class Solution {
+public:
+    
+    int goodNodes(TreeNode* root, int val=INT_MIN) {
+        return root == nullptr ? 0 : (val <= root->val)  + goodNodes(root->left, max(root->val, val)) + goodNodes(root->right, max(root->val, val));
+    }
+};
