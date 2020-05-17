8cfd5f6a81de24358649f70bbf4473e3e30ef4ef
priety33 <52635177+priety33@users.noreply.github.com>
2020-05-17 11:18:53 +0530

    Create 1448. Count Good Nodes in Binary Tree.cpp

---
+++ 1448. Count Good Nodes in Binary Tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    int ans=0;
+    
+    void count(TreeNode* root, vector<int> v)
+    {
+        if(!root) return; 
+        if(v.size()==0) ans++;
+        else if(v[v.size()-1]<=root->val) ans++;
+        v.push_back(root->val);
+        sort(v.begin(), v.end());
+        count(root->left, v);
+        count(root->right, v);
+    }
+    
+    int goodNodes(TreeNode* root) {
+        vector<int> v;
+        count(root, v);
+        return ans;
+    }
+};
