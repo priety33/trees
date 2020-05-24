3337c71795a82db9f3d37f1d23673672b4399e5a
priety33 <52635177+priety33@users.noreply.github.com>
2020-05-24 10:55:35 +0530

    Update 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp

--- 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
+++ 1457. Pseudo-Palindromic Paths in a Binary Tree.cpp
@@ -12,46 +12,32 @@
 class Solution {
 public:
     int count=0;
+    vector<int> p;
+    
+    bool checkpalin() {
+        int odd=0;
+        for(int i=0; i<10; i++) {
+            if(p[i]&1) odd++;
+            if(odd>1) break;
+        }
+        cout<<odd<<" ";
+        return odd<=1;
+    }
+    
+    void helper(TreeNode* root) {
+        if(!root) return;
+        p[root->val]++;
+        if(!root->left && !root->right) {
+            if( checkpalin() ) count++;
+        }
+        helper(root->left);
+        helper(root->right);
+        p[root->val]--;
+    }
+    
     int pseudoPalindromicPaths (TreeNode* root) {
-        if (root == NULL) return 0;
-        stack<TreeNode*> s;
-        stack<vector<int>> pathStack;
-        s.push(root);
-        pathStack.push({root->val});
-        vector<vector<int>> temp;
-        while (!s.empty()) {
-            TreeNode * curNode = s.top(); s.pop();
-            vector<int> tmpPath = pathStack.top(); pathStack.pop();
-            
-            if (curNode->left == NULL && curNode->right == NULL) {
-                temp.push_back(tmpPath);
-                continue;
-            }
-            
-            if (curNode->left != NULL) {
-                s.push(curNode->left);
-                vector<int> x=tmpPath;
-                x.push_back(curNode->left->val);
-                pathStack.push(x);
-            }
-            
-            if (curNode->right != NULL) {
-                s.push(curNode->right);
-                vector<int> x=tmpPath;
-                x.push_back(curNode->right->val);
-                pathStack.push(x);
-            }
-        }
-        for(auto v:temp){
-            unordered_map<int,int> m;
-            int odd=0;
-            for(auto j:v) m[j]++;
-            for(auto j:m){
-                if(j.second%2!=0)odd++;
-                if(odd>1) break;
-            }
-            if(odd<=1) count++;
-        }
+        p.assign(10, 0);
+        helper(root);
         return count;
     }
 };
